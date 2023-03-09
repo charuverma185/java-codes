7767a07985b2b0281f3ac18e85164e7e63d1e026
charuverma185 <114462820+charuverma185@users.noreply.github.com>
2023-03-09 23:06:49 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class min {
+    public static void main(String[] args) {
+        int arr[]={5,1,7,2,8};
+        int n=arr.length;
+        Arrays.sort(arr);
+        System.out.print("minimun number:" + arr[0]);
+    }
+}
