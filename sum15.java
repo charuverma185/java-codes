ae4d8ea8449bf437d5ad1223c83e891627c5fa5e
charuverma185 <114462820+charuverma185@users.noreply.github.com>
2023-03-09 18:42:59 +0530

    Add files via upload

---
+++ sum15.java
@@ -0,0 +1,16 @@
+public class sum15 {
+ public static void main(String[] args) {
+    int a=10;
+    int b=5;
+    int sub,sum;
+    float mul,div;
+    sum = a+b;
+    sub = a-b;
+    mul = a*b;
+    div = a/b;
+    System.out.println(sum);
+    System.out.println(sub);
+    System.out.println(mul);
+    System.out.println(div);
+ }   
+}
